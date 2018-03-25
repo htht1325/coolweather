26e51d7caa6ef4f009e6ad3656fffe99bf428cd6
htht1325 <332592738@qq.com>
2018-03-25 11:08:47 +0800

    third commit

--- app/src/main/java/com/coolweather/mycoolweather/MainActivity.java
+++ app/src/main/java/com/coolweather/mycoolweather/MainActivity.java
@@ -2,11 +2,15 @@ package com.coolweather.mycoolweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainlayout);
+        Button mybtn = (Button)findViewById(R.id.my_btn);
     }
+
 }
